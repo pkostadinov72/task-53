7ffb0b8cdf64debd3eef3dcd434f41e1d8fb0f48
Plamenpowa <pkostadinov72@gmail.com>
2022-11-30 02:29:33 +0200

    setting data-price

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+  const product = document.querySelector("#product");
+  const price = document.querySelector(".price");
+  product.dataset.price = price.textContent;
 });
